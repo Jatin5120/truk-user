7a428a0fe746b45d6bef9bc317d51a66d56d38be
lazycoder1996 <mujappiah@gmail.com>
2020-12-24 19:23:57 +0000

    Update trackShipment.dart

---
+++ lib/screens/trackShipment.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TrackShipment extends StatefulWidget {
+  @override
+  _TrackShipmentState createState() => _TrackShipmentState();
+}
+
+class _TrackShipmentState extends State<TrackShipment> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Track Shipment'),
+      ),
+    );
+  }
+}
